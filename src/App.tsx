eaf457ffca2a59d1478f0748a35df7838ba7fdd8
Guilherme Totti <dev.guilhermetotti@gmail.com>
2024-06-13 01:25:39 -0300

    feat: base config and add dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
-export function App() {
+import { ThemeProvider } from 'styled-components'
+import { defaultTheme } from './styles/themes/default'
+import { Button } from './components/Button'
+import { GlobalStyle } from './styles/global'
 
+export function App() {
   return (
-    <>      
-    </>
+    <ThemeProvider theme={defaultTheme}>
+      <Button variant="primary" />
+      <Button variant="secondary" />
+      <Button variant="success" />
+      <Button variant="danger" />
+      <Button />
+
+      <GlobalStyle />
+    </ThemeProvider>
   )
 }
