c163628083a4ab84335b462986702c8a49478631
Guilherme Totti <dev.guilhermetotti@gmail.com>
2024-06-13 01:28:25 -0300

    feat: delete Button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,10 @@
 import { ThemeProvider } from 'styled-components'
 import { defaultTheme } from './styles/themes/default'
-import { Button } from './components/Button'
 import { GlobalStyle } from './styles/global'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Button variant="primary" />
-      <Button variant="secondary" />
-      <Button variant="success" />
-      <Button variant="danger" />
-      <Button />
-
       <GlobalStyle />
     </ThemeProvider>
   )
